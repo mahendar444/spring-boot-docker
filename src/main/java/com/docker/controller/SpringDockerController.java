eb5c8b481e179aa4626c51629c9423c5c49bfcf0
BAJJURI MAHENDAR REDDY <61425460+mahendar444@users.noreply.github.com>
2022-06-16 20:02:22 +0530

    Project Added

---
+++ src/main/java/com/docker/controller/SpringDockerController.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.docker.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author MAHENDAR REDDY
+ *
+ */
+@RestController
+public class SpringDockerController {
+	
+	@GetMapping(value = "hello")
+	public String getHelloDocker() {
+		return "Hello SpringBoot Docker Demo project....";
+	}
+
+}
